9ae5b0009657b2d6c3b138ef55f942f4aa928c40
tmkzflintz <sakunthep.c@kisracorp.com>
2019-08-06 11:54:46 +0700

    update

--- bot-flex.php
+++ bot-flex.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'ouhqskdRP/sUP8uwpjAadPDJz6rj1Y3IR0/ZznmHBgsPmYq6Q+hzdEJ4OXgyw/8NaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c0XIOEKRfUEQAsHVi4PbQU4HEk9GOq/cmdR3iRkQE9e5gdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
+$ACCESS_TOKEN = 'd6lvdWM3/fAEgcvEw0ZGscWDcIM5lBhq6frtQmwsgr3sJ2Q21mYoDhLGlXyPL91AVG7NlgGlbIGcCVb7MS2WFnHUcwzOTo9fEQ8UNST6JcCYipoTstoX4AxQYtWmacGCVTLeMCN3CJatd6siZ0kHNQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '71816dc43fab6ca16a206795cf04d3a7';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
